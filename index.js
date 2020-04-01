4200c6c6b5f867ec27f65081e249b274f2027588
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2020-04-01 17:51:59 +0200

    DEBUG: remove

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
+  console.log('start')
   const labelsToFailString = core.getInput("labels-to-fail");
   const myToken = core.getInput("repo-token");
 
@@ -15,8 +16,12 @@ try {
   // myToken: ${{ secrets.GITHUB_TOKEN }}
   // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
 
+  
+  console.log('getting octokit')
   const octokit = new github.GitHub(myToken);
   const { owner, repo } = github.context.repo;
+  console.log('running checks')
+  console.log('start of token', myToken.slice(0,3))
   octokit.checks
     .listForRef({
       owner,
